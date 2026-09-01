3e0732913e4e8ce69c34753bb90e9a8a273e7b85
Fahmida Afroz Moni <fahmidaafrozmoni2928@gmail.com>
2026-09-01 18:29:39 +0600

    middileware changes

--- src/middleware.js
+++ src/middleware.js
@@ -1,22 +1,15 @@
+// 
+
+
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-    
-    const sessionToken = request.cookies.get("better-auth.session_token")?.value;
-
-    
-    if (!sessionToken) {
-        return NextResponse.redirect(new URL("/login", request.url));
-    }
-
-    
-    return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: [
-        "/all-appointment/:id*",
-        "/dashboard/:path*",
-    ],
+  matcher: [
+    "/all-appointment/:id*",
+    "/dashboard/:path*",
+  ],
 };
-
